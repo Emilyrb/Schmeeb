9b8618a93f4f3cd985fbf4e231394cce507881eb
emily-guan <emily.guan27@hotmail.com>
2023-09-11 23:50:25 +1000

    Start add expense form

--- src/Components/AddExpenseDialog.tsx
+++ src/Components/AddExpenseDialog.tsx
@@ -1,4 +1,8 @@
-import { Button, Modal } from "react-bootstrap";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import styled from "styled-components";
 
 interface Props {
   addExpenseDialog: string;
@@ -6,6 +10,7 @@ interface Props {
 }
 
 export function AddExpenseDialog(props: Props) {
+  const [ memberList, setMemberList ] = useState(['']); 
   const { addExpenseDialog, setAddExpenseDialog } = props;
   const handleClose = () => setAddExpenseDialog(null);
 
@@ -14,7 +19,7 @@ export function AddExpenseDialog(props: Props) {
       <Modal.Header closeButton>
         <Modal.Title>Add expense</Modal.Title>
       </Modal.Header>
-      <Modal.Body>form goes here, activity id is: {addExpenseDialog}</Modal.Body>
+      <Modal.Body>{renderForm({memberList, setMemberList})} {addExpenseDialog}</Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Close
@@ -24,5 +29,47 @@ export function AddExpenseDialog(props: Props) {
         </Button>
       </Modal.Footer>
     </Modal>
+  );
+}
+
+interface memberDataProps {
+  memberList: string[];
+  setMemberList: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const MemberInput = styled.div`
+  width: 90%;
+  display: inline-block;
+`;
+
+function renderForm(props: memberDataProps) {
+  const { memberList, setMemberList } = props;
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="form.name">
+        <Form.Label>Activity Name</Form.Label>
+        <Form.Control type="text" placeholder="Movie night" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="form.description">
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Form.Label>Members: <span><FontAwesomeIcon icon={faPlus} onClick={() => {setMemberList(memberList => [...memberList, ''] )}} size={'2x'} /></span></Form.Label>
+      {Array.from(Array(memberList.length), (e, i) => {
+        return <AddedMemmber {...props} />
+      })}
+    </Form>
+  );
+}
+
+function AddedMemmber(props: memberDataProps) {
+  const { memberList, setMemberList } = props;
+
+  return (
+    <Form.Group className="mb-3" controlId="form.member">
+      <MemberInput><Form.Control type="text" placeholder="Carmy" /></MemberInput>
+      <span><FontAwesomeIcon icon={faMinus} onClick={() => {}} size={'2x'} /></span>
+    </Form.Group>
   );
 }
